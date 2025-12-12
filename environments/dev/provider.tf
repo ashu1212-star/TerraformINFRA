ef38e9765579d1796b8b307493c805fa2e895e79
ashu <ashutosh.pal26@gmail.com>
2025-12-12 20:40:11 +0530

    backend added

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -5,6 +5,14 @@ terraform {
         version = "4.54.0"
     }
   }
+
+
+backend "azurerm" {
+  resource_group_name  = "rg-dev"
+  storage_account_name = "stg1example"
+  container_name       = "pravin"
+  key                  = "dev.terraform.tfstate"
+}
 }
 
 provider "azurerm" {
